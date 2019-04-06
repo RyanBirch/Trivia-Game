d0f6af2356f9d4063c7a04bc42d45d1e0c9eaec5
Ryan Birch <ryan.birch@gmail.com>
2019-04-06 01:32:46 -0400

    began work on timing events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         'How many colonies fought in the American Revolution?',
         'Who did America fight a war of independence against?',
         'What year was the Declaration of Independence drafted?',
-        'Which country was a major ally to the United States during the war?'
+        'Which country was a major ally to the United States during the Revolutionary War?'
       ]
 
 
@@ -31,6 +31,7 @@ $(document).ready(function() {
     }
   ]
 
+  // initial game state
   let userGuess
   let currentQ = 0
 
@@ -41,15 +42,72 @@ $(document).ready(function() {
   $('#answer-3').html(answers[currentQ].ansArr[3])
 
 
-  $('#answer-0').on('click', function() {
-    userGuess = answers[currentQ].ansArr[0]
+  // questions will flip every 30 seconds
+  let tq = setInterval(timedQ, 1000 * 2)
+
+  // executes if the user doesn't answer in time
+  function timedQ() {
+    if (currentQ < 4) currentQ++
+    else clearInterval(tq)
+    nextQ()
+    $('alert').html('Time\'s Up')
+    console.log(currentQ)
+  }
+
+  // checks if  answer is correct
+  function checkAnswer() {
     if (userGuess === answers[currentQ].ansCorrect) {
       $('#alert').html('Correct!')
     } else {
       $('#alert').html('Wrong!')
     }
+  }
 
-    currentQ++
+  // flips to next question
+  function nextQ() {
+    $('#question').html(questions[currentQ])
+    $('#answer-0').html(answers[currentQ].ansArr[0])
+    $('#answer-1').html(answers[currentQ].ansArr[1])
+    $('#answer-2').html(answers[currentQ].ansArr[2])
+    $('#answer-3').html(answers[currentQ].ansArr[3])
+    }
+
+
+
+  $('#answer-0').on('click', function() {
+    userGuess = answers[currentQ].ansArr[0]
+    checkAnswer()
+    if (currentQ < 4) currentQ++
+    nextQ()
+    clearInterval(tq)
+    console.log(currentQ)
+  })
+
+  $('#answer-1').on('click', function() {
+    userGuess = answers[currentQ].ansArr[1]
+    checkAnswer()
+    if (currentQ < 4) currentQ++
+    nextQ()
+    clearInterval(tq)
+    console.log(currentQ)
+  })
+
+  $('#answer-2').on('click', function() {
+    userGuess = answers[currentQ].ansArr[2]
+    checkAnswer()
+    if (currentQ < 4) currentQ++
+    nextQ()
+    clearInterval(tq)
+    console.log(currentQ)
+  })
+
+  $('#answer-3').on('click', function() {
+    userGuess = answers[currentQ].ansArr[3]
+    checkAnswer()
+    if (currentQ < 4) currentQ++
+    nextQ()
+    clearInterval(tq)
+    console.log(currentQ)
   })
 
 
